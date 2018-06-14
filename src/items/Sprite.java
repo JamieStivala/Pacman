025648170b4932f07b5a8389fe65207f1f49ad72
Jamie Stivala <jamiestivala@gmail.com>
2018-06-14 18:36:18 +0200

    Added setters to the sprite

--- src/items/Sprite.java
+++ src/items/Sprite.java
@@ -1,5 +1,23 @@
 package items;
 
 public class Sprite extends Blob{
+    public Sprite(String resourceName, String format, boolean visible, int x, int y, int width, int height) {
+        super(resourceName, format, visible, x, y, width, height);
+    }
 
+    public void setX(int x){
+        area.setLocation(x, getY());
+    }
+
+    public void setY(int y){
+        area.setLocation(getX(), y);
+    }
+
+    public void setWidth(int width){
+        area.setSize(width, getHeight());
+    }
+
+    public void setHeight(int height){
+        area.setSize(getWidth(), height);
+    }
 }

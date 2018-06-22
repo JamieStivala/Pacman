88ad7064cbd429ee7dc8a12d01ef3fa3b6e23825
Jamie Stivala <jamiestivala@gmail.com>
2018-06-22 16:33:52 +0200

    Added an algorithm that splits the grid of buttons in rows

--- src/PacmanFrame.java
+++ src/PacmanFrame.java
@@ -1,8 +1,11 @@
+import generator.Seed;
+
 import javax.swing.*;
 import java.awt.*;
 
 class PacmanFrame extends Frame {
     private GridLayout gridLayout = new GridLayout(20, 400);
+    Seed seed;
     PacmanFrame(){
         super("PacmanRunner");
         super.setSize(640, 480);
@@ -11,10 +14,20 @@ class PacmanFrame extends Frame {
         super.setBackground(Color.BLACK);
         super.setLayout(gridLayout);
 
-        for(int i = 0; i != 800; i++){
-            super.add(new Button(i + ""));
-        }
+        seed = new Seed();
 
+        for(int i = 0, rowCounter = 0; i != 800; i++){
+            if(i % 40 == 0) {
+                rowCounter++;
+                Button button = new Button("split");
+                button.setBackground(Color.blue);
+                super.add(button);
+            }else{
+                super.add(new Button(i + ""));
+            }
+
+
+        }
         super.paint(super.getGraphics());
     }
 }

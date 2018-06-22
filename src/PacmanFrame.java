54fac90949ef6a6581c6797a3dc580c60b7176c3
Jamie Stivala <jamiestivala@gmail.com>
2018-06-22 15:22:40 +0200

    Added a grid layout and made it filled with buttons for now

--- src/PacmanFrame.java
+++ src/PacmanFrame.java
@@ -1,10 +1,20 @@
+import javax.swing.*;
 import java.awt.*;
 
 class PacmanFrame extends Frame {
+    private GridLayout gridLayout = new GridLayout(20, 400);
     PacmanFrame(){
         super("PacmanRunner");
-        super.setSize(640, 460);
+        super.setSize(640, 480);
+        super.setExtendedState(JFrame.MAXIMIZED_BOTH);
         super.setVisible(true);
         super.setBackground(Color.BLACK);
+        super.setLayout(gridLayout);
+
+        for(int i = 0; i != 800; i++){
+            super.add(new Button(i + ""));
+        }
+
+        super.paint(super.getGraphics());
     }
 }

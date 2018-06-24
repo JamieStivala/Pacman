c6420148aafe08a5d0bbfcb82100eef834217cd2
Jamie Stivala <jamiestivala@gmail.com>
2018-06-24 12:16:03 +0200

    Added a method to fix the length of the string

--- src/generator/BinaryOperations.java
+++ src/generator/BinaryOperations.java
@@ -2,5 +2,13 @@ package generator;
 
 public class BinaryOperations {
 
+    private static String fixLength(String fixing){
+        StringBuilder buffer = new StringBuilder(fixing);
+        if (fixing.length() == 8) return fixing;
+        for(int i = 0; i != 8 - fixing.length(); i++){
+            buffer.insert(0, 0);
+        }
 
+        return buffer.toString();
+    }
 }

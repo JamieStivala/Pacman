c36a62e344114ff03d642a5befdb9f94d0b71ce4
Jamie Stivala <jamiestivala@gmail.com>
2018-06-28 10:35:18 +0200

    Seed is going to be a 2D array

--- src/generator/Seed.java
+++ src/generator/Seed.java
@@ -1,29 +1,28 @@
 package generator;
 
 public class Seed {
-    private long seed;
     private int split[];
+    private boolean seed[][];
 
     public Seed() {
         this((long) (Math.random() * 1000000000));
     }
 
     public Seed(long seed){
-        this.seed = seed;
         this.split = new int[20];
         System.out.println(seed);
 
-        String temp = Long.toString(this.seed);
+        String temp = Long.toString(seed);
         for(int i = 0; i != temp.length(); i++) {
             split[i] = temp.charAt(i) - '0';
         }
 
-        temp = Long.toString(this.seed * 2);
+        temp = Long.toString(seed * 2);
         for(int i = 8; i != temp.length() + 8; i++){
             split[i] = temp.charAt(i - 8) - '0';
         }
 
-        temp = Long.toString(this.seed * 3);
+        temp = Long.toString(seed * 3);
         for(int i = 17; i != 20; i++){
             split[i] = temp.charAt(i - 17) - '0';
         }

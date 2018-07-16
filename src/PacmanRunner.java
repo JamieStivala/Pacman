7d921e7a8dcab50e75132331f1d437797490984a
Jamie Stivala <jamiestivala@gmail.com>
2018-07-16 23:41:38 +0200

    Removed useless comments

--- src/PacmanRunner.java
+++ src/PacmanRunner.java
@@ -1,10 +1,5 @@
 public class PacmanRunner {
     public static void main (String args []){
-        /*
-        PacmanRunner:
-        Item | -> User -> Thread that constantly moves the User
-             | -> Bot
-         */
         PacmanFrame pacmanFrame = new PacmanFrame();
     }
 }

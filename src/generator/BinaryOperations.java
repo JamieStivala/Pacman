53b5e649b80846f5d43fc0384401f046f753f296
Jamie Stivala <jamiestivala@gmail.com>
2018-07-21 17:27:21 +0200

    Implemented an algorithm where
    00 -> empty
    01 -> coin
    10 -> coin
    11 -> wall

--- src/generator/BinaryOperations.java
+++ src/generator/BinaryOperations.java
@@ -15,4 +15,29 @@ public class BinaryOperations {
         }
         return buffer.toString();
     }
+
+    static BlockType[] binaryToEnum(int firstNumber, int secondNumber, int thirdNumber){
+        String binaryString = fixLength(Integer.toBinaryString(firstNumber), 30)
+                + fixLength(Integer.toBinaryString(secondNumber), 30)
+                + fixLength(Integer.toBinaryString(thirdNumber), 20);
+
+        char binary[] = binaryString.toCharArray();
+        BlockType blockType[] = new BlockType[40];
+
+        for(int i = 0, counter = 0; i != binary.length; i++, counter++){
+            char defaultI = binary[i];
+            char incrementedI = binary[++i];
+
+            if(defaultI == '0' && incrementedI == '0'){
+                blockType[counter] = BlockType.EMPTY;
+            }else if(defaultI == '0' && incrementedI == '1'){
+                blockType[counter] = BlockType.COIN;
+            }else if(defaultI == '1' && incrementedI == '0'){
+                blockType[counter] = BlockType.COIN;
+            }else if(defaultI == '1' && incrementedI == '1'){
+                blockType[counter] = BlockType.WALL;
+            }
+        }
+        return blockType;
+    }
 }

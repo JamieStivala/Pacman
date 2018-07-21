33d84675a30bbd1adedfec481bbe14a52c1e144a
Jamie Stivala <jamiestivala@gmail.com>
2018-07-21 17:39:54 +0200

    Implemented the binary operations to work with the new seed class

--- src/generator/Seed.java
+++ src/generator/Seed.java
@@ -4,20 +4,29 @@ import java.util.Random;
 
 class Seed {
     private Random random;
-    private boolean seed[][];
+    private BlockType seed[][];
 
     Seed() {
         this((long) (Math.random() * 1000000000));
     }
 
     Seed(long seed) {
-        this.seed = new boolean[20][40];
+        this.seed = new BlockType[20][40];
         this.random = new Random(seed);
-    }
-}
 
-enum BlockType{
-    EMPTY,
-    COIN,
-    WALL
+        for(int vertical = 0; vertical != 20; vertical++){
+            this.seed[vertical] = BinaryOperations.numberToEnum(
+                    random.nextInt(1000000000),
+                    random.nextInt(1000000000),
+                    random.nextInt(1000000000));
+        }
+    }
+
+    public BlockType[][] getSeed() {
+        return seed;
+    }
+
+    public BlockType[] getSeed(int index) {
+        return seed[index];
+    }
 }

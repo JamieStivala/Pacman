529db030269edde0b3a22871e2e5bf02372f4b3b
Jamie Stivala <jamiestivala@gmail.com>
2018-07-21 17:24:44 +0200

    Fix Length can now be given a specific length

--- src/generator/BinaryOperations.java
+++ src/generator/BinaryOperations.java
@@ -1,11 +1,16 @@
 package generator;
 
 public class BinaryOperations {
-    private static String fixLength(String fixing){
-        if (fixing.length() == 30) return fixing;
+    private static String fixLength(String fixing, int length) {
+        if (fixing.length() == length) return fixing;
         StringBuilder buffer = new StringBuilder(fixing);
-        for(int i = 0; i != 30 - fixing.length(); i++){
-            buffer.insert(0, 0);
+
+        if (fixing.length() < length) {
+            for (int i = 0; i != length - fixing.length(); i++) {
+                buffer.insert(0, 0);
+            }
+        } else {
+            buffer.delete(0, fixing.length() - length);
         }
 
         return buffer.toString();

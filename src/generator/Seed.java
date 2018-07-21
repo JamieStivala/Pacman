9d0b64a43149c4cd1ea00bbb907a32e4f414ec09
Jamie Stivala <jamiestivala@gmail.com>
2018-07-21 16:35:16 +0200

    Rewriting the seed start

--- src/generator/Seed.java
+++ src/generator/Seed.java
@@ -1,37 +1,18 @@
 package generator;
 
+import java.util.Random;
+
 class Seed {
+    private Random random;
     private boolean seed[][];
 
-    Seed(long seed){
-        int split[] = new int[20];
-        this.seed = new boolean[20][40];
-        System.out.println(seed);
-
-        int currentCounter = 0;
-        String temp = Long.toString(seed);
-
-        for(; currentCounter != temp.length(); currentCounter++){
-            split[currentCounter] = temp.charAt(currentCounter) - '0';
-        }
-
-        temp = Long.toString(seed * 2);
-        for(int i = 0; i != temp.length(); i++, currentCounter++){
-            split[currentCounter] = temp.charAt(i) - '0';
-        }
-
-        temp = Long.toString(seed * 3);
-        for(int i = 0; currentCounter != split.length; i++, currentCounter++){
-            split[currentCounter] = temp.charAt(i) - '0';
-        }
-
-        for (int i = 0; i != split.length; i++) {
-            this.seed[i] = BinaryOperations.binaryToBoolean(BinaryOperations.oneDecimalToMultipleBinary(split[i]));
-        }
+    Seed(){
+        this((long) (Math.random() * 1000000000) );
     }
 
-    public boolean[] getSeed(int index){
-        return this.seed[index];
+    Seed(long seed){
+        this.seed = new boolean[20][40];
+        this.random = new Random(seed);
     }
 }
 

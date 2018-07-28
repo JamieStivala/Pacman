e16ead584c132ca2d322b3eac24a64fcfbc3d743
Jamie Stivala <jamiestivala@gmail.com>
2018-07-28 14:17:23 +0200

    Added a method that does the spawn box

--- src/generator/PacmanMap.java
+++ src/generator/PacmanMap.java
@@ -7,5 +7,49 @@ public class PacmanMap extends Seed {
 
     public PacmanMap(long seed){
         super(seed);
+        this.addSpawnBox();
+    }
+
+    private void addSpawnBox(){
+        //Side walls
+        super.getSeed(7)[19] = BlockType.WALL;
+        super.getSeed(7)[20] = BlockType.WALL;
+        super.getSeed(7)[21] = BlockType.WALL;
+
+        super.getSeed(11)[19] = BlockType.WALL;
+        super.getSeed(11)[20] = BlockType.WALL;
+        super.getSeed(11)[21] = BlockType.WALL;
+        //Side walls
+
+        //Top & Bottom walls
+        super.getSeed(8)[22] = BlockType.WALL;
+        super.getSeed(9)[22] = BlockType.WALL;
+        super.getSeed(10)[22] = BlockType.WALL;
+
+        super.getSeed(8)[18] = BlockType.WALL;
+        super.getSeed(9)[18] = BlockType.WALL;
+        super.getSeed(10)[18] = BlockType.WALL;
+        //Top & Bottom walls
+
+        //Empty the outside box
+        super.getSeed(11)[18] = BlockType.EMPTY;
+        super.getSeed(7)[18] = BlockType.EMPTY;
+        super.getSeed(11)[22] = BlockType.EMPTY;
+        super.getSeed(7)[22] = BlockType.EMPTY;
+        //Empty the outside box
+
+        //Empty the inside box
+        super.getSeed(8)[19] = BlockType.EMPTY;
+        super.getSeed(8)[20] = BlockType.EMPTY;
+        super.getSeed(8)[21] = BlockType.EMPTY;
+
+        super.getSeed(9)[19] = BlockType.EMPTY;
+        super.getSeed(9)[20] = BlockType.EMPTY;
+        super.getSeed(9)[21] = BlockType.EMPTY;
+
+        super.getSeed(10)[19] = BlockType.EMPTY;
+        super.getSeed(10)[20] = BlockType.EMPTY;
+        super.getSeed(10)[21] = BlockType.EMPTY;
+        //Empty the inside box
     }
 }

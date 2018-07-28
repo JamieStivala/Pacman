f8f4557494623b66c6f75cc57f35abf0412d704e
Jamie Stivala <jamiestivala@gmail.com>
2018-07-28 12:23:50 +0200

    Starting work on making the PacmanMap

--- src/generator/PacmanMap.java
+++ src/generator/PacmanMap.java
@@ -1,24 +1,11 @@
 package generator;
 
-import items.Blob;
-import items.stationery.Coin;
-
 public class PacmanMap extends Seed {
-    Blob items[][] = new Blob[20][40];
-
-    PacmanMap(){
+    public PacmanMap(){
         this((long) (Math.random() * 1000000000));
     }
 
-    PacmanMap(long seed){
+    public PacmanMap(long seed){
         super(seed);
-
-        for(int vertical = 0; vertical != super.getSeed().length; vertical++){
-            for(int horizontal = 0; horizontal != super.getSeed(vertical).length; horizontal++){
-                if(super.getSeed(vertical)[horizontal] == BlockType.COIN){
-                    items[vertical][horizontal] = new Coin();
-                }
-            }
-        }
     }
 }

b37cbf5da469aa390d696a8e6782f3144eb835c2
Jamie Stivala <jamiestivala@gmail.com>
2018-08-02 22:36:58 +0200

    Added somethings to the PacmanKeyListener such as Pacman object and JFrame object

--- src/listener/PacmanKeyListener.java
+++ src/listener/PacmanKeyListener.java
@@ -1,9 +1,18 @@
 package listener;
 
+import items.moving.Pacman;
+
+import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class PacmanKeyListener implements KeyListener {
+    private Pacman pacman;
+    private JFrame jFrame;
+    public PacmanKeyListener(Pacman pacman, JFrame pacmanFrame){
+        this.pacman = pacman;
+        this.jFrame = pacmanFrame;
+    }
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -11,7 +20,12 @@ public class PacmanKeyListener implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == 37 || e.getKeyCode() == 65) pacman.moveLeft(5);
+        else if(e.getKeyCode() == 39 || e.getKeyCode() == 68) pacman.moveRight(5);
+        else if(e.getKeyCode() == 38 || e.getKeyCode() == 87) pacman.moveUp(5);
+        else if (e.getKeyCode() == 40 || e.getKeyCode() == 83) pacman.moveDown(5);
 
+        jFrame.update(jFrame.getGraphics());
     }
 
     @Override

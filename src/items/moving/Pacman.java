3ddc6b79f0b1b9bfb4b6609846106b085e09ec57
Jamie Stivala <jamiestivala@gmail.com>
2018-08-03 00:14:44 +0200

    Added rotations and getters and setters to the pacman object

--- src/items/moving/Pacman.java
+++ src/items/moving/Pacman.java
@@ -3,7 +3,17 @@ package items.moving;
 import items.Sprite;
 
 public class Pacman extends Sprite {
+    private PacmanRotation rotation;
+
     public Pacman(int x, int y) {
         super("pacman.png", x, y, 35, 35);
+    }
+
+    public PacmanRotation getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(PacmanRotation rotation) {
+        this.rotation = rotation;
     }
 }

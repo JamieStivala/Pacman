a9349f52693eaefa97c725d8da951c0e65ac699d
Jamie Stivala <jamiestivala@gmail.com>
2018-08-02 22:36:33 +0200

    Added the pacman to the PacmanFrame

--- src/PacmanFrame.java
+++ src/PacmanFrame.java
@@ -1,3 +1,4 @@
+import items.moving.Pacman;
 import listener.PacmanKeyListener;
 import listener.PacmanWindowListener;
 import map.PacmanMap;
@@ -5,18 +6,19 @@ import map.PacmanMap;
 import javax.swing.*;
 import java.awt.*;
 
-class PacmanFrame extends JFrame{
+public class PacmanFrame extends JFrame{
     private GridLayout gridLayout = new GridLayout(20, 40);
     private PacmanMap map;
+    private Pacman pacman;
 
     PacmanFrame(){
         map = new PacmanMap(937870171);
-
+        pacman = new Pacman(2, 24);
         super.setTitle("Pacman");
         super.setSize(1440, 799);
         super.setResizable(true);
         super.addWindowListener(new PacmanWindowListener());
-        super.addKeyListener(new PacmanKeyListener());
+        super.addKeyListener(new PacmanKeyListener(pacman, this));
         super.setBackground(new Color(5, 19, 28));
         super.setLayout(null);
         super.setVisible(true);
@@ -25,6 +27,11 @@ class PacmanFrame extends JFrame{
     @Override
     public void paint(Graphics g){
         map.paint(g);
+        pacman.paint(g);
     }
 
+    @Override
+    public void update(Graphics g){
+        paint(g);
+    }
 }

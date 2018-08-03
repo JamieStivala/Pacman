9f0e47ee1a821f7d1f6f28d79c961fbbc4316fd3
Jamie Stivala <jamiestivala@gmail.com>
2018-08-03 19:54:21 +0200

    Dead-end on previous algorithm, need to come up with a new one

--- src/map/WallCollisionDetection.java
+++ src/map/WallCollisionDetection.java
@@ -3,7 +3,6 @@ package map;
 import frames.PacmanFrame;
 import items.Blob;
 import items.moving.Pacman;
-import items.moving.PacmanRotation;
 
 import java.util.ArrayList;
 
@@ -11,6 +10,7 @@ public class WallCollisionDetection extends Thread {
     private PacmanFrame frame;
     private Pacman pacman;
     private volatile boolean keyPressedSinceCollision = false;
+
     public WallCollisionDetection(PacmanFrame frame) {
         this.frame = frame;
         this.pacman = frame.getPacman();
@@ -18,21 +18,14 @@ public class WallCollisionDetection extends Thread {
 
     @Override
     public void run() {
-        System.out.println(keyPressedSinceCollision);
         while (frame.isRunning()) {
             ArrayList<Blob> wall = frame.getMap().getOrganizedBlocks().get(BlockType.WALL);
             for (Blob blob : wall) {
 
-                if(!keyPressedSinceCollision || pacman.hasCollidedWith(blob)) {
-                    if(pacman.getRotation() == PacmanRotation.RIGHT && blob.getX() > pacman.getX()) {
-                        keyPressedSinceCollision = false;
-                        pacman.setCollidedWithWall(true);
-                    }else if (pacman.getRotation() == PacmanRotation.LEFT && pacman.getX() > blob.getX()){
-                        keyPressedSinceCollision = false;
-                        pacman.setCollidedWithWall(true);
-                    }
-                }else {
-                    pacman.setCollidedWithWall(false);
+                if(!pacman.hasCollidedWith(blob)) {
+                    System.out.println(pacman.getArea().x + " " + pacman.getArea().y);
+                    System.out.println(blob.getArea().x + " " + blob.getArea().y);
+                    System.out.println("\n\n\n\n");
                 }
             }
         }

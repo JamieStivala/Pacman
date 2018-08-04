dd588e5de6b6e0b813d1c8880364691c3662bd26
Jamie Stivala <jamiestivala@gmail.com>
2018-08-04 17:27:07 +0200

    Starting work on adding the logo

--- src/frames/MainMenu.java
+++ src/frames/MainMenu.java
@@ -2,27 +2,26 @@ package frames;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class MainMenu extends JFrame {
-    private BufferedImage background;
+    private BufferedImage logo;
+    private JButton startGame;
     public MainMenu() {
-        try {
-            background = ImageIO.read(new File("resources/" + "PacmanBackground.png"));
-        } catch (Exception e) {
-            System.out.println("Error loading: PacmanBackground");
-        }
-
+        this.loadTextures();
         super.setTitle("Pacman - Main Menu");
         super.setSize(1440, 799);
         super.setVisible(true);
-        paint(super.getGraphics());
+        super.setLayout(null);
+        super.getContentPane().getGraphics().drawImage(logo, 208, 0, null);
     }
 
-    @Override
-    public void paint(Graphics g){
-        g.drawImage(background, 0, 0, null);
+    private void loadTextures(){
+        try {
+            logo = ImageIO.read(new File("resources/menu/PacmanLogo.png"));
+        } catch (Exception e) {
+            System.out.println("Error loading: Pacman Logo");
+        }
     }
 }

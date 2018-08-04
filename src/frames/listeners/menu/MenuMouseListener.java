ee0a34e27257d36be9642d35808f6459e48c7b49
Jamie Stivala <jamiestivala@gmail.com>
2018-08-04 19:08:24 +0200

    Made the texture for startGame expand when the mouse is over it

--- src/frames/listeners/menu/MenuMouseListener.java
+++ src/frames/listeners/menu/MenuMouseListener.java
@@ -1,9 +1,16 @@
 package frames.listeners.menu;
 
+import frames.MainMenu;
+
+import javax.swing.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class MenuMouseListener implements MouseListener {
+    private MainMenu mainMenu;
+    public MenuMouseListener(MainMenu mainMenu) {
+        this.mainMenu = mainMenu;
+    }
 
     @Override
     public void mouseClicked(MouseEvent e) {
@@ -17,16 +24,19 @@ public class MenuMouseListener implements MouseListener {
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        if(e.getComponent().getName().equals(""));
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-
+        if(e.getComponent() == mainMenu.getStartGame()){
+            mainMenu.getStartGame().setIcon(new ImageIcon(mainMenu.getStartGameTextures()[1]));
+        }
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-
+        if(e.getComponent() == mainMenu.getStartGame()){
+            mainMenu.getStartGame().setIcon(new ImageIcon(mainMenu.getStartGameTextures()[0]));
+        }
     }
 }

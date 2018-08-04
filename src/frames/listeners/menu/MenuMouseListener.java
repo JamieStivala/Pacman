1c5f6bfba487ba10e9669c347949171a678d580d
Jamie Stivala <jamiestivala@gmail.com>
2018-08-04 17:42:06 +0200

    Added a menu mouse listener

---
+++ src/frames/listeners/menu/MenuMouseListener.java
@@ -0,0 +1,32 @@
+package frames.listeners.menu;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MenuMouseListener implements MouseListener {
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        if(e.getComponent().getName().equals(""));
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}

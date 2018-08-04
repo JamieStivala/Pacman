9d5c498047f20b5f80086f8dbe9bf61b365d1b02
Jamie Stivala <jamiestivala@gmail.com>
2018-08-04 23:11:08 +0200

    Renamed newGameTextures to newProfileTextures

--- src/frames/panels/menu/NewGameMenu.java
+++ src/frames/panels/menu/NewGameMenu.java
@@ -6,11 +6,13 @@ import frames.listeners.menu.MenuMouseListener;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class NewGameMenu extends BasePanel {
-    private ImageIcon newGameTextures[];
+    private ImageIcon newProfileTextures[];
+    private BufferedImage noSaveFound;
     private JLabel newProfile;
     public NewGameMenu(MainMenu mainMenu){
         super(mainMenu);
@@ -19,15 +21,15 @@ public class NewGameMenu extends BasePanel {
     @Override
     void loadComponents() {
         newProfile = new JLabel();
-        newProfile.setBounds(560, 299, 400, 40);
-        newProfile.setIcon(newGameTextures[0]);
+        newProfile.setBounds(544, 399, 352, 32);
+        newProfile.setIcon(newProfileTextures[0]);
         newProfile.addMouseListener(new MenuMouseListener(super.getMainMenu()));
         super.add(newProfile);
     }
 
     @Override
     void loadTextures() {
-        newGameTextures = new ImageIcon[2];
+        newProfileTextures = new ImageIcon[2];
         try{
             newGameTextures[0] = new ImageIcon(ImageIO.read(new File("resources/menu/textures/profile/new_small.png")));
             newGameTextures[1] = new ImageIcon(ImageIO.read(new File("resources/menu/textures/profile/new_big.png")));
@@ -41,8 +43,8 @@ public class NewGameMenu extends BasePanel {
         super.paint(g);
     }
 
-    public ImageIcon[] getNewGameTextures() {
-        return newGameTextures;
+    public ImageIcon[] getNewProfileTextures() {
+        return newProfileTextures;
     }
 
     public JLabel getNewProfile() {

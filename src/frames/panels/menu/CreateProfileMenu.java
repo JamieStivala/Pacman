ee46884433bb8c0e3d240af1e719df71d3d20322
Jamie Stivala <jamiestivala@gmail.com>
2018-08-04 23:59:42 +0200

    Added a CreateProfileMenu

---
+++ src/frames/panels/menu/CreateProfileMenu.java
@@ -0,0 +1,19 @@
+package frames.panels.menu;
+
+import frames.MainMenu;
+
+public class CreateProfileMenu extends BasePanel {
+    public CreateProfileMenu(MainMenu mainMenu) {
+        super(mainMenu);
+    }
+
+    @Override
+    void loadComponents() {
+
+    }
+
+    @Override
+    void loadTextures() {
+
+    }
+}

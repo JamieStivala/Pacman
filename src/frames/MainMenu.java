9f5b6339aaaed6dfc4b5bb93b2dd4322358d52ee
Jamie Stivala <jamiestivala@gmail.com>
2018-08-04 19:06:20 +0200

    Added a paint and a proper way of drawing in the logo

--- src/frames/MainMenu.java
+++ src/frames/MainMenu.java
@@ -1,5 +1,7 @@
 package frames;
 
+import frames.listeners.menu.MenuMouseListener;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -9,16 +11,21 @@ import java.io.File;
 public class MainMenu extends JFrame {
     private BufferedImage logo;
     private BufferedImage[] startGameTextures;
-    private JButton startGame;
+    private JLabel startGame;
     public MainMenu() {
         this.loadTextures();
         super.setTitle("Pacman - Main Menu");
         super.setSize(1440, 799);
         super.setVisible(true);
-        super.setLayout(null);
+        super.getContentPane().setLayout(null);
         super.getContentPane().setBackground(new Color(240, 130, 0));
-        super.getContentPane().getGraphics().drawImage(logo, 208, 0, null);
-        addButtons();
+        addComponents();
+        repaint();
+    }
+
+    public void paint( Graphics g ) {
+        super.paint(g);
+        if (logo != null) g.drawImage(logo, 208, 20, 1024, 242, null);
     }
 
     private void loadTextures(){

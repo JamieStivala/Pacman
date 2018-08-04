4b61d6ee68af22ee8944ffc8e53ccdb02ceabc6f
Jamie Stivala <jamiestivala@gmail.com>
2018-08-04 20:52:08 +0200

    Added methods to load and save the user

---
+++ src/user/manager/UserHandler.java
@@ -0,0 +1,31 @@
+package user.manager;
+
+import java.io.*;
+import java.util.ArrayList;
+
+@SuppressWarnings("unchecked")
+public class UserHandler {
+    public static void saveUser(ArrayList<User> users){
+        try{
+            ObjectOutputStream userSaver = new ObjectOutputStream(new FileOutputStream("saves/users.sav"));
+            userSaver.writeObject(users);
+            userSaver.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<User> loadUser(){
+        if(!new File("saves/users.sav").exists()) return null;
+
+        ArrayList<User> users = null;
+        try{
+            ObjectInputStream f = new ObjectInputStream(new FileInputStream("saves/users.sav"));
+            users = (ArrayList<User>) f.readObject();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return users;
+    }
+}

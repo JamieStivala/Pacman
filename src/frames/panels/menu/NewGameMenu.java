ca1bab5d940b5a112de033222d17f2e55ca4db9a
Jamie Stivala <jamiestivala@gmail.com>
2018-08-04 22:19:26 +0200

    Loaded textures on NewGameMenu

--- src/frames/panels/menu/NewGameMenu.java
+++ src/frames/panels/menu/NewGameMenu.java
@@ -2,26 +2,37 @@ package frames.panels.menu;
 
 import frames.MainMenu;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class NewGameMenu extends BasePanel {
+    private ImageIcon newGameTextures[];
     public NewGameMenu(MainMenu mainMenu){
         super(mainMenu);
+    }
+
+    @Override
+    void loadComponents() {
+    }
+
+    @Override
+    void loadTextures() {
+        newGameTextures = new ImageIcon[2];
+        try{
+            newGameTextures[0] = new ImageIcon(ImageIO.read(new File("resources/menu/textures/profile/new_small.png")));
+            newGameTextures[1] = new ImageIcon(ImageIO.read(new File("resources/menu/textures/start/new_big.png")));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void paint(Graphics g){
         super.paint(g);
         if (super.getMainMenu().getLogo() != null) g.drawImage(super.getMainMenu().getLogo(), 208, 20, 1024, 242, null);
-    }
-
-    @Override
-    void loadComponents() {
-
-    }
-
-    @Override
-    void loadTextures() {
-
+        if (super.getMainMenu().getLogo() != null) g.drawImage(super.getMainMenu().getLogo(), 208, 100, 1024, 242, null);
     }
 }

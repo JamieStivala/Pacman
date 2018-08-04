c7464847142f6cab579a38dd51662dd8ec6994f4
Jamie Stivala <jamiestivala@gmail.com>
2018-08-04 22:46:27 +0200

    BasePanel now also sets the background

--- src/frames/panels/menu/BasePanel.java
+++ src/frames/panels/menu/BasePanel.java
@@ -9,6 +9,7 @@ abstract class BasePanel extends JPanel {
     private MainMenu mainMenu;
     BasePanel(MainMenu mainMenu){
         this.mainMenu = mainMenu;
+        super.setBackground(new Color(240, 130, 0));
         loadTextures();
         loadComponents();
         super.repaint();
@@ -19,7 +20,7 @@ abstract class BasePanel extends JPanel {
     @Override
     public void paint(Graphics g){
         super.paint(g);
-        if (mainMenu.getLogo() != null) g.drawImage(mainMenu.getLogo(), 208, 20, 1024, 242, null);
+        if (mainMenu.getLogo() != null) g.drawImage(mainMenu.getLogo(), 208, 0, 1024, 242, null);
     }
 
     MainMenu getMainMenu() {

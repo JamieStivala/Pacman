1fa5252343c2fb38d587f8778854dce66aedc048
Jamie Stivala <jamiestivala@gmail.com>
2018-08-04 20:37:50 +0200

    Added a User class that will store the User Score

---
+++ src/UserManager/User.java
@@ -0,0 +1,38 @@
+package UserManager;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class User implements Serializable {
+    private String characterName;
+    private int totalScore;
+    private ArrayList<Long> seedsPlayed;
+
+    public User(String characterName){
+        this.characterName = characterName;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(int totalScore) {
+        this.totalScore = totalScore;
+    }
+
+    public ArrayList<Long> getSeedsPlayed() {
+        return seedsPlayed;
+    }
+
+    public void setSeedsPlayed(ArrayList<Long> seedsPlayed) {
+        this.seedsPlayed = seedsPlayed;
+    }
+}

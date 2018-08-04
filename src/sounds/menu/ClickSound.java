b0462290c3ffe458156644a8a058ea0b11dfce41
Jamie Stivala <jamiestivala@gmail.com>
2018-08-04 21:37:11 +0200

    Added a thread to play the click sound

---
+++ src/sounds/menu/ClickSound.java
@@ -0,0 +1,19 @@
+package sounds.menu;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+public class ClickSound extends Thread {
+    public void run(){
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("resources/menu/sounds/click.wav").getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}

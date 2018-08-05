0f43764334262cc825cdfa7362dd9a8843918181
Jamie Stivala <jamiestivala@gmail.com>
2018-08-06 01:28:23 +0200

    Starting work on the formatting of the PlayGame Panel

--- src/frames/panels/menu/PlayGame.java
+++ src/frames/panels/menu/PlayGame.java
@@ -2,7 +2,25 @@ package frames.panels.menu;
 
 import frames.MainMenu;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 public class PlayGame extends BasePanel {
+    private BufferedImage enterSeed;
+    private BufferedImage or;
+
+    private JLabel previousSeedLabel;
+    private ImageIcon previousSeedTextures[];
+
+    private JLabel startLabel;
+    private ImageIcon startTextures[];
+
+    private JLabel generateRandomWorldLabel;
+    private ImageIcon generateRandomWorldTextures[];
+
     public PlayGame(MainMenu mainMenu) {
         super(mainMenu, true);
     }
@@ -10,10 +28,44 @@ public class PlayGame extends BasePanel {
     @Override
     void loadComponents() {
         super.loadComponents();
+
+        this.generateRandomWorldLabel = new JLabel();
+        this.generateRandomWorldLabel.setIcon(generateRandomWorldTextures[0]);
+        this.generateRandomWorldLabel.setBounds(50, 299, 672, 32);
+        super.add(generateRandomWorldLabel);
+
+        this.startLabel = new JLabel();
+        this.startLabel.setIcon(startTextures[0]);
+        this.startLabel.setBounds(640, 650, 160, 40);
+        super.add(startLabel);
     }
 
     @Override
     void loadTextures() {
         super.loadTextures();
+        this.previousSeedTextures = new ImageIcon[2];
+        this.startTextures = new ImageIcon[2];
+        this.generateRandomWorldTextures = new ImageIcon[2];
+        try{
+            this.previousSeedTextures[0] = new ImageIcon(ImageIO.read(new File("resources/menu/textures/start/seed/previous_seed_small.png")));
+            this.previousSeedTextures[1] = new ImageIcon(ImageIO.read(new File("resources/menu/textures/start/seed/previous_seed_big.png")));
+
+            this.generateRandomWorldTextures[0] = new ImageIcon(ImageIO.read(new File("resources/menu/textures/start/seed/generate_random_world_small.png")));
+            this.generateRandomWorldTextures[1] = new ImageIcon(ImageIO.read(new File("resources/menu/textures/start/seed/generate_random_world_big.png")));
+
+            this.startTextures[0] = new ImageIcon(ImageIO.read(new File("resources/menu/textures/start/start_small.png")));
+            this.startTextures[1] = new ImageIcon(ImageIO.read(new File("resources/menu/textures/start/start_big.png")));
+
+            this.enterSeed = ImageIO.read(new File("resources/menu/textures/start/seed/enter_seed.png"));
+            this.or = ImageIO.read(new File("resources/menu/textures/start/seed/or.png"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawImage(enterSeed, 50, 299, null);
     }
 }

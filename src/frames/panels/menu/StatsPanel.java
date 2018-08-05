2f525ad17fb075582ac185b48db2fedce6323e00
Jamie Stivala <jamiestivala@gmail.com>
2018-08-05 17:32:00 +0200

    Starting work on the StatsPanel

---
+++ src/frames/panels/menu/StatsPanel.java
@@ -0,0 +1,19 @@
+package frames.panels.menu;
+
+import frames.MainMenu;
+
+public class StatsPanel extends BasePanel {
+    public StatsPanel(MainMenu mainMenu) {
+        super(mainMenu);
+    }
+
+    @Override
+    void loadComponents() {
+
+    }
+
+    @Override
+    void loadTextures() {
+
+    }
+}

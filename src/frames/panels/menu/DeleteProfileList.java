26c6a6012660f84921a9f89ea0aa73ff54bb7e87
Jamie Stivala <jamiestivala@gmail.com>
2018-08-05 22:37:15 +0200

    Working on a DeleteProfileList

---
+++ src/frames/panels/menu/DeleteProfileList.java
@@ -0,0 +1,9 @@
+package frames.panels.menu;
+
+import frames.MainMenu;
+
+public class DeleteProfileList extends LoadProfileList {
+    public DeleteProfileList(MainMenu mainMenu) {
+        super(mainMenu);
+    }
+}

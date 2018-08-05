14da8a30d570bd6429fef7dcfebf771411f6e1bf
Jamie Stivala <jamiestivala@gmail.com>
2018-08-05 12:25:33 +0200

    Loaded the listOfProfiles texture

--- src/frames/panels/menu/LoadProfileList.java
+++ src/frames/panels/menu/LoadProfileList.java
@@ -2,18 +2,28 @@ package frames.panels.menu;
 
 import frames.MainMenu;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class LoadProfileList extends BasePanel {
+    private BufferedImage listOfProfiles;
     public LoadProfileList(MainMenu mainMenu) {
         super(mainMenu);
     }
 
     @Override
     void loadComponents() {
-
     }
 
     @Override
     void loadTextures() {
-
+        try {
+            listOfProfiles = ImageIO.read(new File("resources/textures/profile/list_of_profiles.png"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }

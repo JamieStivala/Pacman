24bce0009372ef0b3b77ce827661e90cbea6aafb
Jamie Stivala <jamiestivala@gmail.com>
2018-08-05 21:46:35 +0200

    If the back button is pressed, go back

--- src/frames/panels/menu/BasePanel.java
+++ src/frames/panels/menu/BasePanel.java
@@ -79,6 +79,7 @@ abstract class BasePanel extends JPanel implements MouseListener {
     @Override
     public void mouseClicked(MouseEvent e) {
         if(e.getComponent() instanceof JLabel) new ClickSound().start();
+        if(e.getComponent() == this.backLabel) mainMenu.goBack();
     }
 
     @Override

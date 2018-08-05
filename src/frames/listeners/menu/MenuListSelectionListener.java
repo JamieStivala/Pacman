35840cce6c6383b79b4a6cf4e889eac6dd7ca52e
Jamie Stivala <jamiestivala@gmail.com>
2018-08-05 14:55:50 +0200

    Added a constructor and it has to pass MainMenu

--- src/frames/listeners/menu/MenuListSelectionListener.java
+++ src/frames/listeners/menu/MenuListSelectionListener.java
@@ -1,11 +1,18 @@
 package frames.listeners.menu;
 
+import frames.MainMenu;
 import sounds.menu.ClickSound;
 
+import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 public class MenuListSelectionListener implements ListSelectionListener {
+    private MainMenu mainMenu;
+    public MenuListSelectionListener(MainMenu mainMenu) {
+        this.mainMenu = mainMenu;
+    }
+
     @Override
     public void valueChanged(ListSelectionEvent e) {
         if(e.getValueIsAdjusting()) new ClickSound().start();

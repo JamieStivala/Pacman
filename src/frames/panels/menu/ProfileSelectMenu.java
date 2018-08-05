5c46b90ffa7a844853485ff55ea4993423164836
Jamie Stivala <jamiestivala@gmail.com>
2018-08-05 03:10:06 +0200

    Starting work on the ProfileSelectMenu

---
+++ src/frames/panels/menu/ProfileSelectMenu.java
@@ -0,0 +1,19 @@
+package frames.panels.menu;
+
+import frames.MainMenu;
+
+public class ProfileSelectMenu extends BasePanel {
+    public ProfileSelectMenu(MainMenu mainMenu) {
+        super(mainMenu);
+    }
+
+    @Override
+    void loadComponents() {
+
+    }
+
+    @Override
+    void loadTextures() {
+
+    }
+}

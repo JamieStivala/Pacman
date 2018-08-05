999f628411e8de1cedc5971b6b85eecf3fdc9bbd
Jamie Stivala <jamiestivala@gmail.com>
2018-08-05 13:02:16 +0200

    Added the LoadProfileList list

--- src/frames/panels/menu/LoadProfileList.java
+++ src/frames/panels/menu/LoadProfileList.java
@@ -3,6 +3,7 @@ package frames.panels.menu;
 import frames.MainMenu;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -10,12 +11,28 @@ import java.io.IOException;
 
 public class LoadProfileList extends BasePanel {
     private BufferedImage listOfProfiles;
+    private JList<String> profiles;
+
     public LoadProfileList(MainMenu mainMenu) {
         super(mainMenu);
     }
 
     @Override
     void loadComponents() {
+        DefaultListModel<String> listModel= new DefaultListModel<>();
+        for (int i = 0; i != super.getMainMenu().getUsers().size(); i++) {
+            listModel.add(i, super.getMainMenu().getUsers().get(i).getCharacterName());
+        }
+
+        profiles = new JList<>(listModel);
+        profiles.setBounds(312, 310, 826, 400);
+        profiles.setBackground(new Color(240, 130, 0));
+        profiles.setFont(super.getFont());
+        profiles.setFixedCellHeight(70);
+        DefaultListCellRenderer renderer = (DefaultListCellRenderer) profiles.getCellRenderer();
+        renderer.setHorizontalAlignment(SwingConstants.CENTER);
+
+        super.add(profiles);
     }
 
     @Override

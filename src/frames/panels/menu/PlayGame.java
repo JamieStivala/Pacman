70c6eb9c229a1c6adcf0256f29da1637e7bea7ab
Jamie Stivala <jamiestivala@gmail.com>
2018-08-05 22:04:56 +0200

    Added a PlayGame.java

---
+++ src/frames/panels/menu/PlayGame.java
@@ -0,0 +1,19 @@
+package frames.panels.menu;
+
+import frames.MainMenu;
+
+public class PlayGame extends BasePanel {
+    public PlayGame(MainMenu mainMenu) {
+        super(mainMenu, true);
+    }
+
+    @Override
+    void loadComponents() {
+        super.loadComponents();
+    }
+
+    @Override
+    void loadTextures() {
+        super.loadTextures();
+    }
+}

b671f90c934bdb88f241ea3cf12a4c2f1eed1b88
Jamie Stivala <jamiestivala@gmail.com>
2018-08-05 02:43:47 +0200

    Added a MenuKeyListener()

---
+++ src/frames/listeners/menu/MenuKeyListener.java
@@ -0,0 +1,37 @@
+package frames.listeners.menu;
+
+import frames.MainMenu;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.regex.Pattern;
+
+public class MenuKeyListener implements KeyListener {
+    private MainMenu mainMenu;
+    private Pattern pattern;
+    public MenuKeyListener (MainMenu mainMenu){
+        pattern = Pattern.compile("[^ a-z]", Pattern.CASE_INSENSITIVE);
+        this.mainMenu = mainMenu;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //All the keys that are used while typing and don't have a character
+        boolean isTypingKey = e.isActionKey() || e.getKeyCode() == 20 || e.getKeyCode() == 16 || e.getKeyCode() == 17 || e.getKeyCode() == 18 || e.getKeyCode() == 157 || e.getKeyCode() == 8 || e.getKeyCode() == 65406 || e.getKeyCode() == 27;
+        if(!isTypingKey && e.getSource() == mainMenu.getCreateProfileMenu().getEnterProfileName() && pattern.matcher(e.getKeyChar() + "").find()){
+            String currentText = mainMenu.getCreateProfileMenu().getEnterProfileName().getText();
+            mainMenu.getCreateProfileMenu().getEnterProfileName().setText(currentText.substring(0, currentText.length() - 1));
+        }
+
+    }
+}

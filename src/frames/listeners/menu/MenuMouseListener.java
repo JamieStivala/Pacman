5b4ac0230d1ba89f986da0adbfd646d7ea2f23c4
Jamie Stivala <jamiestivala@gmail.com>
2018-08-05 02:58:56 +0200

    Made the animation thing

--- src/frames/listeners/menu/MenuMouseListener.java
+++ src/frames/listeners/menu/MenuMouseListener.java
@@ -39,6 +39,9 @@ public class MenuMouseListener implements MouseListener {
         if(e.getComponent() == mainMenu.getNewProfileMenu().getNewProfile()){
             mainMenu.getNewProfileMenu().getNewProfile().setBounds(340, 399, 760, 40);
             mainMenu.getNewProfileMenu().getNewProfile().setIcon(mainMenu.getNewProfileMenu().getNewProfileTextures()[1]);
+        }else if(e.getComponent() == mainMenu.getCreateProfileMenu().getCreateProfile()){
+            mainMenu.getCreateProfileMenu().getCreateProfile().setBounds(600, 410, 240, 40);
+            mainMenu.getCreateProfileMenu().getCreateProfile().setIcon(mainMenu.getCreateProfileMenu().getCreateProfileTextures()[1]);
         }
     }
 
@@ -47,6 +50,9 @@ public class MenuMouseListener implements MouseListener {
         if(e.getComponent() == mainMenu.getNewProfileMenu().getNewProfile()){
             mainMenu.getNewProfileMenu().getNewProfile().setBounds(416, 399, 608, 32);
             mainMenu.getNewProfileMenu().getNewProfile().setIcon(mainMenu.getNewProfileMenu().getNewProfileTextures()[0]);
+        }else if(e.getComponent() == mainMenu.getCreateProfileMenu().getCreateProfile()){
+            mainMenu.getCreateProfileMenu().getCreateProfile().setBounds(624, 410, 192, 32);
+            mainMenu.getCreateProfileMenu().getCreateProfile().setIcon(mainMenu.getCreateProfileMenu().getCreateProfileTextures()[0]);
         }
     }
 }

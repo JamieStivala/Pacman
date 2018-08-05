884f172d63481bac112fcad52cfb265d87155f20
Jamie Stivala <jamiestivala@gmail.com>
2018-08-05 15:40:26 +0200

    Starting work on the main panel

---
+++ src/frames/panels/menu/MainPanel.java
@@ -0,0 +1,19 @@
+package frames.panels.menu;
+
+import frames.MainMenu;
+
+public class MainPanel extends BasePanel {
+    public MainPanel(MainMenu mainMenu) {
+        super(mainMenu);
+    }
+
+    @Override
+    void loadComponents() {
+
+    }
+
+    @Override
+    void loadTextures() {
+
+    }
+}

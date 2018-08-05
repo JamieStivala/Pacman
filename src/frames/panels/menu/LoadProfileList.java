d6252613f0afb7ea86a4895b3128ad7f16d493e9
Jamie Stivala <jamiestivala@gmail.com>
2018-08-05 12:17:35 +0200

    Starting a load profile list

---
+++ src/frames/panels/menu/LoadProfileList.java
@@ -0,0 +1,19 @@
+package frames.panels.menu;
+
+import frames.MainMenu;
+
+public class LoadProfileList extends BasePanel {
+    public LoadProfileList(MainMenu mainMenu) {
+        super(mainMenu);
+    }
+
+    @Override
+    void loadComponents() {
+
+    }
+
+    @Override
+    void loadTextures() {
+
+    }
+}

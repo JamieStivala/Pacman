03f7e155a3b468f1fe6bbd25adc675bbb9a42db4
Jamie Stivala <jamiestivala@gmail.com>
2018-08-05 11:09:09 +0200

    Adding in some textures

--- src/frames/panels/menu/ProfileSelectMenu.java
+++ src/frames/panels/menu/ProfileSelectMenu.java
@@ -2,7 +2,14 @@ package frames.panels.menu;
 
 import frames.MainMenu;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.io.File;
+
 public class ProfileSelectMenu extends BasePanel {
+    private ImageIcon newProfileTextures[];
+    private ImageIcon loadProfileTextures[];
+
     public ProfileSelectMenu(MainMenu mainMenu) {
         super(mainMenu);
     }
@@ -14,6 +21,14 @@ public class ProfileSelectMenu extends BasePanel {
 
     @Override
     void loadTextures() {
+        try{
+            newProfileTextures[0] = new ImageIcon(ImageIO.read(new File("resources/menu/textures/profile/new/new_profile_small.png")));
+            newProfileTextures[1] = new ImageIcon(ImageIO.read(new File("resources/menu/textures/profile/new/new_profile_big.png")));
 
+            loadProfileTextures[0] = new ImageIcon(ImageIO.read(new File("resources/menu/textures/profile/load/load__profile_small.png")));
+            loadProfileTextures[1] = new ImageIcon(ImageIO.read(new File("resources/menu/textures/profile/load/load_profile_big.png")));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }

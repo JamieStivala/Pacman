706d0bd8585604ec66d4558235a597802392dbe2
Jamie Stivala <jamiestivala@gmail.com>
2018-08-06 02:57:47 +0200

    Pattern textPattern from pattern

--- src/frames/listeners/menu/MenuKeyListener.java
+++ src/frames/listeners/menu/MenuKeyListener.java
@@ -8,9 +8,9 @@ import java.util.regex.Pattern;
 
 public class MenuKeyListener implements KeyListener {
     private MainMenu mainMenu;
-    private Pattern pattern;
+    private Pattern textPattern;
     public MenuKeyListener (MainMenu mainMenu){
-        this.pattern = Pattern.compile("[^ a-z]", Pattern.CASE_INSENSITIVE);
+        this.textPattern = Pattern.compile("[^ a-z]", Pattern.CASE_INSENSITIVE);
         this.mainMenu = mainMenu;
     }
 
@@ -30,7 +30,7 @@ public class MenuKeyListener implements KeyListener {
         boolean isTypingKey = e.getKeyChar() == 65535 || e.getKeyCode() == 8 || e.getKeyCode() == 10;
         if(e.getSource() == this.mainMenu.getCreateProfileMenu().getEnterProfileNameTextField()) {
             this.mainMenu.getCreateProfileMenu().getEnterProfileNameTextField().setText(this.mainMenu.getCreateProfileMenu().getEnterProfileNameTextField().getText().toLowerCase());
-            if (!isTypingKey && pattern.matcher(e.getKeyChar() + "").find()) {
+            if (!isTypingKey && textPattern.matcher(e.getKeyChar() + "").find()) {
                 String currentText = this.mainMenu.getCreateProfileMenu().getEnterProfileNameTextField().getText();
                 this.mainMenu.getCreateProfileMenu().getEnterProfileNameTextField().setText(currentText.substring(0, currentText.length() - 1));
             }

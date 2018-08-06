719f41fd80d2568f4985ecfc4b5ee8738150704a
Jamie Stivala <jamiestivala@gmail.com>
2018-08-06 03:30:59 +0200

    Added a custom mouse pressed

--- src/frames/panels/menu/SeedViewerList.java
+++ src/frames/panels/menu/SeedViewerList.java
@@ -1,8 +1,11 @@
 package frames.panels.menu;
 
 import frames.MainMenu;
+import sounds.menu.ClickSound;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 
 public class SeedViewerList extends BasePanel {
     public SeedViewerList(MainMenu mainMenu) {
@@ -22,5 +25,15 @@ public class SeedViewerList extends BasePanel {
     @Override
     public void paint(Graphics g) {
         super.paint(g);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if(e.getComponent() instanceof JLabel) new ClickSound().start();
+        if(e.getComponent() == super.getBackLabel()) {
+            super.getMainMenu().goBack();
+            super.getMainMenu().setPreviousFrame(Panel.MAIN_PANEL);
+        }
+
     }
 }

50515c128fe22d2b1860c20ea626e136bc7ae2f0
Jamie Stivala <jamiestivala@gmail.com>
2018-08-06 15:31:35 +0200

    Coin collision detection now increments score

--- src/map/CoinCollisionDetection.java
+++ src/map/CoinCollisionDetection.java
@@ -6,6 +6,7 @@ import items.moving.Pacman;
 public class CoinCollisionDetection extends Thread {
     private PacmanFrame pacmanFrame;
     private Pacman pacman;
+    private int score = 0;
     private volatile boolean coinUpdateSinceLastBuffer;
 
     public CoinCollisionDetection(PacmanFrame pacmanFrame){
@@ -18,6 +19,7 @@ public class CoinCollisionDetection extends Thread {
         while(pacmanFrame.isRunning()){
             pacmanFrame.getMap().getOrganizedBlocks().get(BlockType.COIN).removeIf(coin ->{
                 if(coin.hasCollidedWith(pacman)){
+                    score++;
                     coinUpdateSinceLastBuffer = false;
                     coin.setVisible(false);
                     return true;
@@ -25,6 +27,7 @@ public class CoinCollisionDetection extends Thread {
                 return false;
             });
         }
+        pacmanFrame.getUser().setLastGameScore(score);
     }
 
     public boolean hasCoinBeenTaken(){

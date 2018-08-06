6af73d3252c5f71f308b17be1eb4bdb9976a6c15
Jamie Stivala <jamiestivala@gmail.com>
2018-08-06 03:28:56 +0200

    Moved everything from mouseClicked to mousePressed

--- src/frames/panels/menu/BasePanel.java
+++ src/frames/panels/menu/BasePanel.java
@@ -129,13 +129,13 @@ abstract class BasePanel extends JPanel implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        if(e.getComponent() instanceof JLabel) new ClickSound().start();
-        if(e.getComponent() == this.backLabel) mainMenu.goBack();
+
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-
+        if(e.getComponent() instanceof JLabel) new ClickSound().start();
+        if(e.getComponent() == this.backLabel) mainMenu.goBack();
     }
 
     @Override

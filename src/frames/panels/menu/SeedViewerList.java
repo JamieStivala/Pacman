07bdcf03b13f271081109ed18a5064ab06b8f9ae
Jamie Stivala <jamiestivala@gmail.com>
2018-08-06 03:29:18 +0200

    Starting work on the listing of SeedViewer

---
+++ src/frames/panels/menu/SeedViewerList.java
@@ -0,0 +1,26 @@
+package frames.panels.menu;
+
+import frames.MainMenu;
+
+import java.awt.*;
+
+public class SeedViewerList extends BasePanel {
+    public SeedViewerList(MainMenu mainMenu) {
+        super(mainMenu, true);
+    }
+
+    @Override
+    void loadComponents() {
+        super.loadComponents();
+    }
+
+    @Override
+    void loadTextures() {
+        super.loadTextures();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+}

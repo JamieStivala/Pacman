c703867bd6cf9438efebba256cb7c6b52f7cd2ce
Jamie Stivala <jamiestivala@gmail.com>
2018-08-08 20:59:04 +0200

    Split some arguments into different methods, code clean up

--- src/items/moving/threads/PacmanMover.java
+++ src/items/moving/threads/PacmanMover.java
@@ -16,27 +16,9 @@ public class PacmanMover extends Thread {
     @Override
     public void run() {
         while (pacmanFrame.isRunning()) {
-            PacmanRotation rotation = pacmanFrame.getPacman().getCurrentRotation();
+            doMove(pacmanFrame.getPacman().getCurrentRotation());
+            renderCoins();
 
-            if (pacman.getX() < -5 && rotation == PacmanRotation.LEFT) {
-                pacman.getArea().setLocation(1423, pacman.getY());
-            } else if (pacman.getX() > 1423 && rotation == PacmanRotation.RIGHT) {
-                pacman.getArea().setLocation(-5, pacman.getY());
-            } else if (pacman.getY() < 17 && rotation == PacmanRotation.UP) {
-                pacman.getArea().setLocation(pacman.getX(), 782);
-            } else if (pacman.getY() > 782 && rotation == PacmanRotation.DOWN) {
-                pacman.getArea().setLocation(pacman.getX(), 17);
-            } else if (!pacman.isCollidedWithWall()) {
-                if (rotation == PacmanRotation.LEFT) pacman.moveLeft(6);
-                else if (rotation == PacmanRotation.RIGHT) pacman.moveRight(6);
-                else if (rotation == PacmanRotation.UP) pacman.moveUp(6);
-                else if (rotation == PacmanRotation.DOWN) pacman.moveDown(6);
-            }
-
-            if (pacmanFrame.getCoinCollisionDetection().hasCoinBeenTaken()) {
-                pacmanFrame.getMap().paint();
-                pacmanFrame.getCoinCollisionDetection().updatedCoins();
-            }
             pacmanFrame.render();
             pacmanFrame.repaint();
             try {
@@ -44,6 +26,30 @@ public class PacmanMover extends Thread {
             } catch (InterruptedException e) {
                 System.err.println("Interrupted exception: " + e.getMessage());
             }
+        }
+    }
+
+    private void doMove(PacmanRotation rotation){
+        if (pacman.getX() < -5 && rotation == PacmanRotation.LEFT) {
+            pacman.getArea().setLocation(1423, pacman.getY());
+        } else if (pacman.getX() > 1423 && rotation == PacmanRotation.RIGHT) {
+            pacman.getArea().setLocation(-5, pacman.getY());
+        } else if (pacman.getY() < 17 && rotation == PacmanRotation.UP) {
+            pacman.getArea().setLocation(pacman.getX(), 782);
+        } else if (pacman.getY() > 782 && rotation == PacmanRotation.DOWN) {
+            pacman.getArea().setLocation(pacman.getX(), 17);
+        } else if (!pacman.isCollidedWithWall()) {
+            if (rotation == PacmanRotation.LEFT) pacman.moveLeft(6);
+            else if (rotation == PacmanRotation.RIGHT) pacman.moveRight(6);
+            else if (rotation == PacmanRotation.UP) pacman.moveUp(6);
+            else if (rotation == PacmanRotation.DOWN) pacman.moveDown(6);
+        }
+    }
+
+    private void renderCoins(){
+        if (pacmanFrame.getCoinCollisionDetection().hasCoinBeenTaken()) {
+            pacmanFrame.getMap().paint();
+            pacmanFrame.getCoinCollisionDetection().updatedCoins();
         }
     }
 }

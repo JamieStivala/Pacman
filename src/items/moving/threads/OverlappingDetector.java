9d32a4dc1cc1ffaf3ef94ac1f9f4bb6b15ca99f1
Jamie Stivala <jamiestivala@gmail.com>
2018-08-08 18:57:49 +0200

    Wrote the overlapping detector class

--- src/items/moving/threads/OverlappingDetector.java
+++ src/items/moving/threads/OverlappingDetector.java
@@ -2,6 +2,9 @@ package items.moving.threads;
 
 import frames.PacmanFrame;
 import items.moving.Pacman;
+import map.Line;
+
+import static items.moving.PacmanRotation.*;
 
 public class OverlappingDetector extends Thread{
     private PacmanFrame pacmanFrame;
@@ -15,7 +18,21 @@ public class OverlappingDetector extends Thread{
     @Override
     public void run(){
         while(pacmanFrame.isRunning()){
-            //stuff
+            if(pacman.getCurrentRotation() == LEFT || pacman.getCurrentRotation() == RIGHT && pacman.getNextRotation() == UP || pacman.getNextRotation() == DOWN){
+                for (Line current: pacmanFrame.getMap().getVerticalWallLines()) {
+                    if(!pacman.getArea().intersectsLine(current.getX(), current.getY(), current.getX1(), current.getY1())){
+                        pacman.setCurrentRotation(pacman.getNextRotation());
+                    }
+                }
+            } else if(pacman.getCurrentRotation() == UP || pacman.getCurrentRotation() == DOWN && pacman.getNextRotation() == LEFT || pacman.getNextRotation() == RIGHT){
+                for (Line current: pacmanFrame.getMap().getHorizontalWallLines()) {
+                    if(!pacman.getArea().intersectsLine(current.getX(), current.getY(), current.getX1(), current.getY1())){
+                        pacman.setCurrentRotation(pacman.getNextRotation());
+                    }
+                }
+            }else{
+                pacman.setCurrentRotation(pacman.getNextRotation());
+            }
         }
     }
 }

2b50c35d89fe123ecbf17a8cffa2ddf02a94b111
Jamie Stivala <jamiestivala@gmail.com>
2018-08-08 15:04:32 +0200

    Reformatted code and going to start a willCollideOnRotate method

--- src/items/moving/threads/PacmanMover.java
+++ src/items/moving/threads/PacmanMover.java
@@ -4,57 +4,44 @@ import frames.PacmanFrame;
 import items.moving.Pacman;
 import items.moving.PacmanRotation;
 
-public class PacmanMover extends Thread{
+public class PacmanMover extends Thread {
     private PacmanFrame pacmanFrame;
     private Pacman pacman;
-    private int amountMovedOnX;
-    private int amountMovedOnY;
 
-    public PacmanMover(PacmanFrame pacmanFrame){
+    public PacmanMover(PacmanFrame pacmanFrame) {
         this.pacmanFrame = pacmanFrame;
         this.pacman = pacmanFrame.getPacman();
     }
 
     @Override
     public void run() {
-        while(pacmanFrame.isRunning()) {
+        while (pacmanFrame.isRunning()) {
             PacmanRotation rotation = pacmanFrame.getPacman().getCurrentRotation();
-            if(amountMovedOnX % 36 == 0 && pacman.getX() != 0){
-                System.out.println(pacman.getX());
-            }
-            if(pacman.getX() < -5 && rotation == PacmanRotation.LEFT){
+
+            if (pacman.getX() < -5 && rotation == PacmanRotation.LEFT) {
                 pacman.getArea().setLocation(1423, pacman.getY());
-            }else if(pacman.getX() > 1423 && rotation == PacmanRotation.RIGHT){
-                pacman.getArea().setLocation(-5 , pacman.getY());
-            }else if(pacman.getY() < 17 && rotation == PacmanRotation.UP){
+            } else if (pacman.getX() > 1423 && rotation == PacmanRotation.RIGHT) {
+                pacman.getArea().setLocation(-5, pacman.getY());
+            } else if (pacman.getY() < 17 && rotation == PacmanRotation.UP) {
                 pacman.getArea().setLocation(pacman.getX(), 782);
-            }else if(pacman.getY() > 782 && rotation == PacmanRotation.DOWN){
+            } else if (pacman.getY() > 782 && rotation == PacmanRotation.DOWN) {
                 pacman.getArea().setLocation(pacman.getX(), 17);
-            }else if(!pacman.isCollidedWithWall()) {
-                if (rotation == PacmanRotation.LEFT) {
-                    pacman.moveLeft(6);
-                    amountMovedOnX +=6;
-                } else if (rotation == PacmanRotation.RIGHT) {
-                    pacman.moveRight(6);
-                    amountMovedOnX +=6;
-                } else if (rotation == PacmanRotation.UP) {
-                    pacman.moveUp(6);
-                    amountMovedOnY += 6;
-                } else if (rotation == PacmanRotation.DOWN) {
-                    pacman.moveDown(6);
-                    amountMovedOnY += 6;
-                }
+            } else if (!pacman.isCollidedWithWall()) {
+                if (rotation == PacmanRotation.LEFT) pacman.moveLeft(6);
+                else if (rotation == PacmanRotation.RIGHT) pacman.moveRight(6);
+                else if (rotation == PacmanRotation.UP) pacman.moveUp(6);
+                else if (rotation == PacmanRotation.DOWN) pacman.moveDown(6);
             }
 
-            if(pacmanFrame.getCoinCollisionDetection().hasCoinBeenTaken()) {
+            if (pacmanFrame.getCoinCollisionDetection().hasCoinBeenTaken()) {
                 pacmanFrame.getMap().paint();
                 pacmanFrame.getCoinCollisionDetection().updatedCoins();
             }
             pacmanFrame.render();
             pacmanFrame.repaint();
-            try{
+            try {
                 Thread.sleep(105);
-            }catch (InterruptedException e){
+            } catch (InterruptedException e) {
                 System.err.println("Interrupted exception: " + e.getMessage());
             }
         }

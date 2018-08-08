58c0f2d6c9e060945266e90cd3c2fd5c13c0a6ab
Jamie Stivala <jamiestivala@gmail.com>
2018-08-08 21:57:44 +0200

    The class ghosts now supports all the ghosts

--- src/items/moving/ghosts/Ghosts.java
+++ src/items/moving/ghosts/Ghosts.java
@@ -1,10 +1,40 @@
 package items.moving.ghosts;
 
+import java.awt.*;
+
 public class Ghosts {
     private Pink pink;
     private Red red;
     private Turquoise turquoise;
     private Yellow yellow;
 
+    public Ghosts(){
+        pink = new Pink(720, 399);
+        red = new Red(720, 399);
+        turquoise = new Turquoise(720, 399);
+        yellow = new Yellow(720, 399);
+    }
 
+    public Pink getPink() {
+        return pink;
+    }
+
+    public Red getRed() {
+        return red;
+    }
+
+    public Turquoise getTurquoise() {
+        return turquoise;
+    }
+
+    public Yellow getYellow() {
+        return yellow;
+    }
+
+    public void paint(Graphics g){
+        red.paint(g);
+        pink.paint(g);
+        turquoise.paint(g);
+        yellow.paint(g);
+    }
 }

26b82f72ddab204c27db977ddba6e35ec94c4dd4
Jamie Stivala <jamiestivala@gmail.com>
2018-08-08 21:41:36 +0200

    Made the constructor not public

--- src/map/Line.java
+++ src/map/Line.java
@@ -6,7 +6,7 @@ public class Line {
     private int x1;
     private int y1;
 
-    public Line(int x, int y, int x1, int y1) {
+    Line(int x, int y, int x1, int y1) {
         this.x = x;
         this.y = y;
         this.x1 = x1;

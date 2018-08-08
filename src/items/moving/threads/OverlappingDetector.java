5e77a6cd50d4077f482f3f38136e91d766d92f7a
Jamie Stivala <jamiestivala@gmail.com>
2018-08-08 19:48:47 +0200

    The final version of overlapping detection

--- src/items/moving/threads/OverlappingDetector.java
+++ src/items/moving/threads/OverlappingDetector.java
@@ -6,58 +6,37 @@ import map.Line;
 
 import static items.moving.PacmanRotation.*;
 
-public class OverlappingDetector extends Thread{
+public class OverlappingDetector extends Thread {
     private PacmanFrame pacmanFrame;
     private Pacman pacman;
 
-    public OverlappingDetector(PacmanFrame pacmanFrame){
+    public OverlappingDetector(PacmanFrame pacmanFrame) {
         this.pacmanFrame = pacmanFrame;
         this.pacman = pacmanFrame.getPacman();
     }
 
     @Override
-    public void run(){
-        while(pacmanFrame.isRunning()){
+    public void run() {
+        while (pacmanFrame.isRunning()) {
             boolean overlaps = false;
 
-            if((pacman.getCurrentRotation() == LEFT || pacman.getCurrentRotation() == RIGHT) && (pacman.getNextRotation() == UP || pacman.getNextRotation() == DOWN)) {
+            if ((pacman.getCurrentRotation() == LEFT || pacman.getCurrentRotation() == RIGHT) && (pacman.getNextRotation() == UP || pacman.getNextRotation() == DOWN)) {
                 for (Line current : pacmanFrame.getMap().getVerticalWallLines()) {
                     if (pacman.getArea().intersectsLine(current.getX(), current.getY(), current.getX1(), current.getY1())) {
                         overlaps = true;
                     }
                 }
-            }else if((pacman.getCurrentRotation() == UP || pacman.getCurrentRotation() == DOWN) && (pacman.getNextRotation() == LEFT || pacman.getNextRotation() == RIGHT)) {
+            } else if ((pacman.getCurrentRotation() == UP || pacman.getCurrentRotation() == DOWN) && (pacman.getNextRotation() == LEFT || pacman.getNextRotation() == RIGHT)) {
                 for (Line current : pacmanFrame.getMap().getHorizontalWallLines()) {
                     if (pacman.getArea().intersectsLine(current.getX(), current.getY(), current.getX1(), current.getY1())) {
                         overlaps = true;
                     }
                 }
             }
-            if(!overlaps) {
-                pacman.setCurrentRotation(pacman.getNextRotation());
-                //System.out.println("in");
-            }
 
-            /*
-            boolean overlaps = false;
-            if((pacman.getCurrentRotation() == LEFT || pacman.getCurrentRotation() == RIGHT) && (pacman.getNextRotation() == UP || pacman.getNextRotation() == DOWN)){
-                for (Line current: pacmanFrame.getMap().getVerticalWallLines()) {
-                    if(!pacman.getArea().intersectsLine(current.getX(), current.getY(), current.getX1(), current.getY1())){
-                        overlaps = true;
-                    }
-                }
-            } else if((pacman.getCurrentRotation() == UP || pacman.getCurrentRotation() == DOWN) && (pacman.getNextRotation() == LEFT || pacman.getNextRotation() == RIGHT)){
-                for (Line current: pacmanFrame.getMap().getHorizontalWallLines()) {
-                    if(!pacman.getArea().intersectsLine(current.getX(), current.getY(), current.getX1(), current.getY1())){
-                        overlaps = true;
-                    }
-                }
-            }
-
-            if(!overlaps){
+            if (!overlaps || (pacman.isCollidedWithWall() && pacman.getCurrentRotation() != pacman.getNextRotation())) {
                 pacman.setCurrentRotation(pacman.getNextRotation());
             }
-            */
         }
 
     }

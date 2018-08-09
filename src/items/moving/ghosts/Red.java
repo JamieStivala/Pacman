3ecb8f6f0a20cd954d36842d17958caaacc85c79
Jamie Stivala <jamiestivala@gmail.com>
2018-08-09 22:57:31 +0200

    Added a bunch of code that fixed the pacman following

--- src/items/moving/ghosts/Red.java
+++ src/items/moving/ghosts/Red.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public class Red extends Sprite {
     private List<Node> path;
+    private int changed;
+
     public Red(int x, int y) {
         super("red.png", x, y, 32, 32);
     }
@@ -16,6 +18,30 @@ public class Red extends Sprite {
     }
 
     public void setPath(List<Node> path) {
+        changed = getChanged(path);
         this.path = path;
+    }
+
+    private int getChanged(List<Node> newNodes){
+        List<Node> oldNodes = this.getPath();
+        int oldFound = getChanged();
+        if(oldNodes == null) return 0;
+
+        int min = (oldNodes.size() < newNodes.size()) ? oldNodes.size() : newNodes.size();
+
+        for (int i = 0; i != min ; i++) {
+            Node oldNode = oldNodes.get(i);
+            for (int x = 0; x != min ; x++) {
+                if(oldNode.getRow() != newNodes.get(x).getRow() || oldNode.getCol() != newNodes.get(x).getCol()) if(x >= oldFound){
+                    return x;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    public int getChanged() {
+        return changed;
     }
 }

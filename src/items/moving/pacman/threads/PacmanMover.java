f60c07092341648ff77153e95f4bc98611ed7fea
Jamie Stivala <jamiestivala@gmail.com>
2018-08-10 01:52:08 +0200

    Working on making the ghosts move

--- src/items/moving/pacman/threads/PacmanMover.java
+++ src/items/moving/pacman/threads/PacmanMover.java
@@ -2,6 +2,7 @@ package items.moving.pacman.threads;
 
 import astar.Node;
 import frames.PacmanFrame;
+import items.moving.ghosts.Ghost;
 import items.moving.ghosts.Ghosts;
 import items.moving.ghosts.threads.GhostsCalculator;
 import items.moving.pacman.Pacman;
@@ -24,7 +25,7 @@ public class PacmanMover extends Thread {
         while (pacmanFrame.isRunning()) {
             doPacmanMove(pacmanFrame.getPacman().getCurrentRotation());
             renderCoins();
-            moveGhosts();
+            moveGhosts(0, new Ghost[]{ghosts.getRed(), ghosts.getYellow(), ghosts.getPink(), ghosts.getTurquoise()});
             pacmanFrame.render();
             pacmanFrame.repaint();
             try {
@@ -59,23 +60,30 @@ public class PacmanMover extends Thread {
         freqOfMoves++;
     }
 
-    private Node lastNode;
-    private void moveGhosts() {
+    private Node lastNode[] = new Node[4];
+    private void moveGhost(Ghost ghost, int counter) {
         new GhostsCalculator(this.pacmanFrame).run();
         if(freqOfMoves <= 6) return;
-        if (ghosts.getRed().getPath() == null || ghosts.getRed().getPath().isEmpty())
+        if (ghost.getPath() == null || ghost.getPath().isEmpty())
             return;
 
-        Node currentNode = this.ghosts.getRed().getPath().get(this.ghosts.getRed().getChanged());
-        if(lastNode != null && this.ghosts.getRed().getChanged() + 1 > ghosts.getRed().getPath().size()  && (currentNode.getCol() == lastNode.getCol() && currentNode.getRow() == lastNode.getRow() || pacman.isCollidedWithWall())){
-            this.ghosts.getRed().setChanged(this.ghosts.getRed().getChanged() + 1);
-            currentNode = this.ghosts.getRed().getPath().get(this.ghosts.getRed().getChanged() + 1);
+        Node currentNode = ghost.getPath().get(ghost.getChanged());
+        if(lastNode != null && ghost.getChanged() + 1 > ghost.getPath().size()  && (currentNode.getCol() == lastNode[counter].getCol() && currentNode.getRow() == lastNode[counter].getRow() || pacman.isCollidedWithWall())){
+            ghost.setChanged(ghost.getChanged() + 1);
+            currentNode = ghost.getPath().get(ghost.getChanged() + 1);
         }
 
         int previousPosition[] = getCoordinatesFromPosition(currentNode);
-        ghosts.getRed().getArea().setLocation(previousPosition[0], previousPosition[1]);
-        lastNode = currentNode;
+        ghost.getArea().setLocation(previousPosition[0], previousPosition[1]);
+        lastNode[counter] = currentNode;
+
         freqOfMoves = 0;
+    }
+
+    private void moveGhosts(int counter, Ghost[] ghosts){
+        if(counter == 4) return;
+        moveGhost(ghosts[counter], counter);
+        moveGhosts(++counter, ghosts);
     }
 
     private void renderCoins() {

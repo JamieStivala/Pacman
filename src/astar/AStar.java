5bb261ae162be311aaa1a02314b4a922b9a924de
Jamie Stivala <jamiestivala@gmail.com>
2018-08-09 22:17:48 +0200

    Increased diagonal cost by loads so that it doesn't go diagonal

--- src/astar/AStar.java
+++ src/astar/AStar.java
@@ -7,7 +7,7 @@ import java.util.PriorityQueue;
 
 public class AStar {
     private static int DEFAULT_HV_COST = 10; // Horizontal - Vertical Cost
-    private static int DEFAULT_DIAGONAL_COST = 14;
+    private static int DEFAULT_DIAGONAL_COST = 40;
     private int hvCost;
     private int diagonalCost;
     private Node[][] searchArea;

f9212254b8a7e27b9adb6edd70879d97bbd2fae3
Jamie Stivala <jamiestivala@gmail.com>
2018-08-09 18:40:50 +0200

    Starting work on the Ghosts calculator

---
+++ src/items/moving/ghosts/threads/GhostsCalculator.java
@@ -0,0 +1,18 @@
+package items.moving.ghosts.threads;
+
+import astar.AStar;
+import frames.PacmanFrame;
+import items.moving.ghosts.Ghosts;
+import items.moving.pacman.Pacman;
+
+public class GhostsCalculator extends Thread {
+    private Ghosts ghosts;
+    private Pacman pacman;
+    private AStar aStar;
+
+    public GhostsCalculator(PacmanFrame pacmanFrame){
+        this.pacman = pacmanFrame.getPacman();
+        this.ghosts = pacmanFrame.getGhosts();
+        this.aStar = pacmanFrame.getaStar();
+    }
+}

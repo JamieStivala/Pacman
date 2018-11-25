4131506783923bbef17494204e7367f9fb403502
Jamie Stivala <jamiestivala@gmail.com>
2018-11-25 16:13:35 +0100

    The generator now takes into account all the blocks

--- src/game/Generator.java
+++ src/game/Generator.java
@@ -14,6 +14,11 @@ public class Generator {
         System.out.println(seed);
         this.builtMap  = new BlockType[width][height];
         this.pseudoRandomGenerator = new Random(seed);
+
+        BlockType[] blockTypes = stringToBinary(correctLengthBinaryNumber(pseudoRandomGenerator.nextInt(1000000000)), BlockType.WALL);
+        for (BlockType b : blockTypes) {
+            System.out.println(b);
+        }
     }
 
     private static String correctLengthBinaryNumber(int number){
@@ -22,12 +27,34 @@ public class Generator {
         return binaryNumber;
     }
 
-    private static boolean[] stringToBinary(String binaryNumber){
+    //00 -> PAC_DOT
+    //01 -> PAC_DOT
+    //10 -> FRUIT/PAC_DOT
+    //11 -> PAC_DOT
+    private static BlockType[] stringToBinary(String binaryNumber, BlockType preferredBlock){
         char[] binaryStringC = binaryNumber.toCharArray();
-        boolean[] binary = new boolean[binaryStringC.length];
+        BlockType[] blockTypes = new BlockType[binaryStringC.length/2];
+        int specialBlockCounter = 0;
 
-        for (int i = 0; i != binaryStringC.length ; i++) {
-            binary[i] = binaryStringC[i] != '0';
+        BlockType otherBlock;
+        otherBlock = preferredBlock == BlockType.PAC_DOT ? BlockType.WALL : BlockType.PAC_DOT;
+
+        for (int binaryStringCounter = 0, blockTypeArrayPositionCounter = 0; blockTypeArrayPositionCounter != blockTypes.length ; binaryStringCounter++) {
+            int binaryStringCounterIncremented = binaryStringCounter++;
+
+            if(binaryStringC[binaryStringCounter] == '0' && binaryStringC[binaryStringCounterIncremented] == '0' || binaryStringC[binaryStringCounter] == '0' && binaryStringC[binaryStringCounterIncremented] == '1'){
+                blockTypes[blockTypeArrayPositionCounter] = preferredBlock;
+            }else if(binaryStringC[binaryStringCounter] == '1' && binaryStringC[binaryStringCounterIncremented] == '0'){
+                blockTypes[blockTypeArrayPositionCounter] = otherBlock;
+            }else if(binaryStringC[binaryStringCounter] == '1' && binaryStringC[binaryStringCounterIncremented] == '1' ){
+                if(specialBlockCounter == 0) {
+                    blockTypes[blockTypeArrayPositionCounter] = BlockType.FRUIT;
+                }else{
+                    blockTypes[blockTypeArrayPositionCounter] = BlockType.POWER_PELLET;
+                }
+                specialBlockCounter ++;
+            }
+            blockTypeArrayPositionCounter++;
         }
 
         return blockTypes;

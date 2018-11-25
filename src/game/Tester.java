6bd9e21325802d7c9bce37189f6185c7f3f046a5
Jamie Stivala <jamiestivala@gmail.com>
2018-11-25 16:41:38 +0100

    Added some packages
    Signed-off-by: Jamie Stivala <jamiestivala@gmail.com>

--- src/game/Tester.java
+++ src/game/Tester.java
@@ -1,5 +1,7 @@
 package game;
 
+import game.map.Generator;
+
 public class Tester {
     public static void main(String[] args){
         Generator generator = new Generator(20, 40);
--- src/game/map/Generator.java
+++ src/game/map/Generator.java
@@ -1,4 +1,4 @@
-package game;
+package game.map;
 
 import java.util.Random;
 

c9995ed61448121a11159db7b4fdd1856341ac7d
Jamie Stivala <jamiestivala@gmail.com>
2018-12-09 18:30:51 +0100

    Import removal
    Signed-off-by: Jamie Stivala <jamiestivala@gmail.com>

--- src/Pacman.java
+++ src/Pacman.java
@@ -1,11 +1,11 @@
-import game.PacmanRunner;
+import game.PacmanFrame;
 
 public class Pacman {
     /**
      * This is what will be used as the main class.  It will create an instance of MainMenu and run it
      */
     public static void main(String args[]) {
-        new PacmanRunner();
+        new PacmanFrame();
     }
 }
 

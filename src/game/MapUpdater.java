cae0765eb243939576f6a88b3fce6e13ddba3b04
Jamie Stivala <jamiestivala@gmail.com>
2018-12-09 18:16:50 +0100

    Added a class that will handle all the map updates
    Signed-off-by: Jamie Stivala <jamiestivala@gmail.com>

---
+++ src/game/MapUpdater.java
@@ -0,0 +1,17 @@
+package game;
+
+import game.map.PacmanMap;
+
+public class MapUpdater extends Thread {
+    private PacmanRunner pacmanRunner;
+    private PacmanMap pacmanMap;
+
+    MapUpdater() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
